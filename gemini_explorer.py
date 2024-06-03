7e7e8b8331965b2262e22ad0af9f57c3eb43aeec
Hersh-SS <hersheysandhu@gmail.com>
2024-06-02 20:29:05 -0400

    Task 4 completed

--- gemini_explorer.py
+++ gemini_explorer.py
@@ -3,7 +3,7 @@ import streamlit as st
 from vertexai.preview import generative_models
 from vertexai.preview.generative_models import GenerativeModel, Part, Content, ChatSession
 
-project = "sample-gemini"
+project = "gemini_explorer"
 vertexai.init(project = project)
 
 config = generative_models.GenerationConfig(
@@ -15,3 +15,59 @@ model = GenerativeModel(
     generation_config = config
 )
 chat = model.start_chat()
+
+# helper function to display and send streamlit messages
+def llm_function(chat: ChatSession, query):
+    response = chat.send_message(query)
+    output = response.candidates[0].content.parts[0].text
+
+    with st.chat_message("model"):
+        st.markdown(output)
+
+    st.session_state.messages.append(
+        {
+            "role": "user",
+            "content": query
+        }
+    )
+    st.session_state.messages.append(
+        {
+            "role": "model",
+            "content": output
+        }
+    )
+
+
+
+    st.title("Gemini Explorer")
+
+    # Initialize chat history
+    if "messages" not in st.session_state:
+        st.session_state.messages = []
+    
+    # Display and load to chat history
+    for index, message in enumerate(st.session_state.messages):
+        content = Content(
+            role = message["role"],
+            parts = [ Part.from_text(message["content"]) ]
+        )
+
+        if index != 0:
+            with st.chat_message(message["role"]):
+                st.markdown(message["content"])
+
+        chat.history.append(content)
+
+    # For Initial message startup
+    if len(st.session_state.messages) == 0:
+        st.session_state.messages.append({"role": "model", "content": "Welcome to Gemini Explorer! How can I assist you today?"})
+        with st.chat_message("model"):
+            st.markdown(st.session_state.messages[-1]["content"])
+
+    # For capture user input
+    query = st.chat_input("Gemini Explorer")
+
+    if query:
+        with st.chat_message("user"):
+            st.markdown(query)
+        llm_function(chat, query)
